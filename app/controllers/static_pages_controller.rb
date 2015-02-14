2cd9531031e98b401df6bb5cc3c4f00735ada5fe
Nakul Pathak <nakulpathak3@hotmail.com>
2015-02-13 19:53:25 -0500

    Finished static pages. Chapter 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
+# Because Ruby uses CamelCase for class names (Section 4.4),
+# my preference is to refer to controllers using their CamelCase names, but this is a matter of taste.
+# (Since Ruby filenames typically use snake case,
+# the Rails generator converts CamelCase to snake case using the underscore method.)
+
 class StaticPagesController < ApplicationController
-  def home
+  def home # Since, the home action is empty all the home method does is execute the view corresponding
+  #to this function
   end
 
   def help
+  end
+
+  def about
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  test "should get home" do #Let’s test the Home page by issuing a GET request to the home action
+    # and then making sure we receive a ‘success’ status code in response.
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby On Rails"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby On Rails"
+  end
+
+  test "should get about" do # Test about page by issuing a get request to the about action
+    # and then making sure we receive a 'success' (200) status code in response!
+    get :about #
+    assert_response :success
+    assert_select "title", "About | Ruby On Rails"
   end
 
 end
