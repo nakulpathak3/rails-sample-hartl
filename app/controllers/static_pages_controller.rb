0a76a82718a02a8c013792a7e9a38f294548e1f4
Nakul Pathak <nakulpathak3@hotmail.com>
2015-02-15 21:33:15 -0500

    Added contact page and DRYed static controller test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,35 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby On Rails"
+  end
+
   test "should get home" do #Let’s test the Home page by issuing a GET request to the home action
     # and then making sure we receive a ‘success’ status code in response.
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby On Rails"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby On Rails"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do # Test about page by issuing a get request to the about action
     # and then making sure we receive a 'success' (200) status code in response!
     get :about #
     assert_response :success
-    assert_select "title", "About | Ruby On Rails"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
