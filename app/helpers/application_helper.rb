358c86b41e376af4bf7c0bab4e711ffb60ec45dc
Nakul Pathak <nakulpathak3@hotmail.com>
2015-02-16 01:02:21 -0500

    Added full_title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # Returns properly formatted title irrespective of whether provide() is used or not.
+  def full_title(page_title = '')
+
+    base_title = "Ruby On Rails App"
+
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+
+  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+  end
+
   test "should get home" do #Let’s test the Home page by issuing a GET request to the home action
     # and then making sure we receive a ‘success’ status code in response.
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby On Rails"
+    assert_select "title", "Ruby On Rails App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby On Rails"
+    assert_select "title", "Help | Ruby On Rails App"
   end
 
   test "should get about" do # Test about page by issuing a get request to the about action
     # and then making sure we receive a 'success' (200) status code in response!
     get :about #
     assert_response :success
-    assert_select "title", "About | Ruby On Rails"
+    assert_select "title", "About | Ruby On Rails App"
   end
 
 end
